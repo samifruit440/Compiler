8f9796582a14318be22738581b3f3b5e45a14c49
Sam <samuel.frappier-fortin@polymtl.ca>
2025-12-21 08:32:54 -0500

    gitignore stuff

--- test_output/test_15.s
+++
@@ -1,8 +0,0 @@
-    .text
-    .globl _start
-_start:
-    movl $0x100000, %esi  # Heap pointer
-    movl $400, %eax
-    movl %eax, %ebx     # return value
-    movl $1, %eax      # exit syscall
-    int $0x80
